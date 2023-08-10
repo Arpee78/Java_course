ac17d6f1ef28f8d15a2d5f014b865c2499a191c5
Arpee78 <arpee78@gmail.com>
2023-08-10 13:31:50 +0200

    ex-introjunit

---
+++ src/test/java/introjunit/GentlemanTest.java
@@ -0,0 +1,33 @@
+package introjunit;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class GentlemanTest {
+
+    @Test
+    public void testSayHello() {
+        //given
+        Gentleman gentleman = new Gentleman();
+
+        //when
+        String hello = gentleman.sayHello("John Doe");
+
+        //then
+        assertThat(hello, equalTo("Hello John Doe"));
+
+    }
+
+//    @Test
+//    public void testSayHello2(){
+//        assertThat(new Gentleman().sayHello("John Doe"),equalTo("Hello John Doe"));
+//    }
+
+    @Test
+    public void testSayHelloWithNull() {
+        assertThat(new Gentleman().sayHello(null), equalTo("Hello Anonymous"));
+    }
+
+}
