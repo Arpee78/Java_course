9a3830cca3bee7e5d9edc0910e6bbd988780439f
Arpee78 <arpee78@gmail.com>
2023-08-24 16:58:52 +0200

    ex-exam_sv2_part1_supsup

---
+++ src/main/java/exam_sv2_part1_supsup/absolute/Absolute.java
@@ -0,0 +1,17 @@
+package exam_sv2_part1_supsup.absolute;
+
+public class Absolute {
+
+    public boolean isAbsolute(int number) {
+        int sum = 0;
+
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        return sum == number;
+    }
+
+
+}
--- src/main/java/exam_sv2_part1_supsup/absolute/AbsoluteMain.java
+++ src/main/java/exam_sv2_part1_supsup/absolute/AbsoluteMain.java
@@ -1,13 +1,13 @@
-//package absolute;
-//
-//public class AbsoluteMain {
-//
-//    public static void main(String[] args) {
-//        Absolute absolute = new Absolute();
-//
-//        System.out.println(absolute.isAbsolute(6)); //true
-//        System.out.println(absolute.isAbsolute(8128)); //true
-//        System.out.println(absolute.isAbsolute(7246)); //false
-//        System.out.println(absolute.isAbsolute(9)); //false
-//    }
-//}
+package exam_sv2_part1_supsup.absolute;
+
+public class AbsoluteMain {
+
+    public static void main(String[] args) {
+        Absolute absolute = new Absolute();
+
+        System.out.println(absolute.isAbsolute(6)); //true
+        System.out.println(absolute.isAbsolute(8128)); //true
+        System.out.println(absolute.isAbsolute(7246)); //false
+        System.out.println(absolute.isAbsolute(9)); //false
+    }
+}
