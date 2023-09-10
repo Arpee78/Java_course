7701b0dbdcdf7a9dd49cbe8fd93750cb78c2638a
Arpee78 <arpee78@gmail.com>
2023-09-10 19:57:07 +0200

    ex-interfaces

---
+++ src/main/java/interfaces/simpletasks/SimpleTasks.java
@@ -0,0 +1,29 @@
+package interfaces.simpletasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleTasks implements Runnable {
+
+    private List<String> tasks;
+
+    public SimpleTasks(List<String> tasks) {
+        this.tasks = tasks;
+    }
+
+    public List<String> getTasks() {
+        return tasks;
+    }
+
+    private void nextStep() {
+        tasks.remove(tasks.size() - 1);
+
+    }
+
+    @Override
+    public void run() {
+        while (!tasks.isEmpty()) {
+            nextStep();
+        }
+    }
+}
