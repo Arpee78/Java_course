9d20db7c3fa5c7da494496aac5df0212765c6396
Arpee78 <arpee78@gmail.com>
2023-09-13 16:34:05 +0200

    ex-interfacestaticmethods

---
+++ src/main/java/interfacestaticmethods/vehicle/Bicycle.java
@@ -0,0 +1,21 @@
+package interfacestaticmethods.vehicle;
+
+public class Bicycle implements Vehicle {
+
+    private int numberOfWheels;
+
+    public Bicycle() {
+    }
+
+    public Bicycle(int numberOfWheels) {
+        this.numberOfWheels = numberOfWheels;
+    }
+
+    public int getNumberOfWheels() {
+        return numberOfWheels;
+    }
+
+    public void setNumberOfWheels(int numberOfWheels) {
+        this.numberOfWheels = numberOfWheels;
+    }
+}
